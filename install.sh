5a452389e7e5164d1d09b626fbdf5be46ed67309
ShaneQful <shane_quigley@hotmail.com>
2012-07-15 18:47:55 +0100

    Changed install script so it will install before asking to install ruby and u1

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 path="$(kde4-config --path services)"
 spath="$(echo ${path%:*})"
-kdesudo apt-get install ruby ubuntuone-control-panel-qt
 mkdir -p "$spath"ServiceMenus/Ubuntu\ One/
 cp * "$spath"ServiceMenus/Ubuntu\ One/
+kdesudo apt-get install ruby ubuntuone-control-panel-qt
