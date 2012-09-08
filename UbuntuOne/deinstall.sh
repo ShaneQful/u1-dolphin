306fd7533e4e9912766aba4c825aaa50c262386c
ShaneQful <shane_quigley@hotmail.com>
2012-09-08 16:30:07 +0100

    Remove some files that never should have been added

--- UbuntuOne/deinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-path="$(kde4-config --path services)"
-spath="$(echo ${path%:*})"
-rm -rf "$spath"ServiceMenus/UbuntuOne/
--- UbuntuOne/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-path="$(kde4-config --path services)"
-spath="$(echo ${path%:*})"
-mkdir -p "$spath"ServiceMenus/UbuntuOne/
-cp * "$spath"ServiceMenus/UbuntuOne/
-kdesudo apt-get install ruby ubuntuone-control-panel-qt
